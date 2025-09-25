c211c76799715ff06ab87a9da7d643372a30ec34
TrustPrince5 <eboigbeprince@gmail.com>
2025-09-25 16:01:51 +0100

    project updated

--- src/services/index.js
+++ src/services/index.js
@@ -1,2 +1,3 @@
-export const baseUrl = 'https://ecommerce-test-api-f4d9.onrender.com/api';
+// export const baseUrl = 'https://ecommerce-test-api-f4d9.onrender.com/api';
+export const baseUrl = 'https://peedells-fragrance-backend.onrender.com/api';
 // export const baseUrl = 'http://localhost:3000';
